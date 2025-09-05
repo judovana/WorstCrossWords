378a7bfc41653dec261083bc34b32540af0ab325
Jiri Vanek <jvanek@redhat.com>
2025-09-05 17:34:33 +0200

    improved and fixed transalte
    * main method now can take language
    * transalte method no longer return lists nor remove articles from long
      transations

--- translate.py
+++ translate.py
@@ -1,9 +1,10 @@
 import sys
+from typing import List
 from transformers import M2M100ForConditionalGeneration
 from tokenization_small100 import SMALL100Tokenizer
 
 initialized=False
-articles = {'a': '', 'an':'', 'and':'', 'the':''}
+articles = {'a': '', 'an':'', 'and':'', 'the':'', 'by':''}
 
 
 def removearticles(text):
@@ -31,19 +32,28 @@ def translateTo(text, lang):
     encoded = tokenizer(text, return_tensors="pt")
     generated_tokens = model.generate(**encoded)
     translated=tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
-    s=removearticles(translated[0].lower().strip())
-    return s;
+    if isinstance(translated, List):
+        translated="".join(translated)
+    translated=translated.lower().strip()
+    if not " " in text:
+        translated=removearticles(translated)
+    return translated;
 
 def main():
     if len(sys.argv) == 1:
-        print("at last one word/sentence to transalte expected")
+        print("exactly one word/sentence expected. ")
+        print("If you put more then ne word/sentence, then the first argument is language, and all others are words/sentence to transalte")
+        print("(So for more then one word, language is mandatory)")
         sys.exit(1)
     if len(sys.argv) == 2:
         r = translateTo(sys.argv[1], "en")
         print(r)
-    if len(sys.argv) > 2:
-        for element in sys.argv:
-            r = translateTo(element, "en")
+    if len(sys.argv) == 3:
+        r = translateTo(sys.argv[2], sys.argv[1])
+        print(r)
+    if len(sys.argv) > 3:
+        for element in sys.argv[2:]:
+            r = translateTo(element, sys.argv[1])
             print(element + "->" + r)
 
 if __name__ == "__main__":
