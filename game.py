d35eae45f8ab67a00722d2f6d60c1e1b5b2bce4c
Jiri Vanek <jvanek@redhat.com>
2025-09-06 13:29:44 +0200

    trasnformed game.py to use generateWords properly and have main

--- game.py
+++ game.py
@@ -11,63 +11,80 @@ import pathlib
 import sys
 import re
 
-wordsFile="cs-20-2025-09-05_14:36:15"
-#wordsFile="cs"
-#wordsFile="de"
-#wordsFile="en"
-lang= re.sub('[^a-z]+', '', wordsFile)
-print("lang is "+lang)
-caches.loadCache(lang)
-words=generateWords.readWorlist(wordsFile)
-random.shuffle(words)
-word=words[0]
-word="čert"
-#word="sem"
-print("eg " + word)
-translatedId=caches.getTranslated(lang, word)
-
-explanationFilesTransalted=caches.getFilesFromTransaltedAiExplainCache(lang, translatedId)
-if not explanationFilesTransalted:
-    caches.explainToCache(lang, translatedId)
-    explanationFilesTransalted=caches.getFilesFromTransaltedAiExplainCache(lang, translatedId)
-for file in explanationFilesTransalted:
-    print(file)
-    print(pathlib.Path(file).read_text())
-
-explanationImages=caches.getFilesFromAiImageCache(translatedId)
-if not explanationImages:
-    caches.imageToCache(lang, translatedId)
-    explanationImages=caches.getFilesFromAiImageCache(translatedId)
-for file in explanationImages:
-    print(file)
-    show_image.display_image(file)
-
-qhelp="help exit L ? ?n ?? (? ?n ?? I In II T G Tn TT GG newI newT delIn delTn)[a-z]"
-history=[];
-comandsUsage = {}
-print(qhelp)
-for line in sys.stdin:
+def main():
+    print("optional first argument is  argument file with all words. Optional second argument may follow - number of words.") 
+    print("WARNING If no first first argument is given, weird `cs` lang is loaded") 
+    print("WARNING the language is detected from file name. Be sure the only letters in the filename are identifying the lang as `cs` `en` or `de`") 
+    print("environment variable "+generateWords.SIZE_VAR+" in format WxH may be used to set size of  desk (be carefull)") 
+    wordsFile="cs-20-2025-09-05_14:36:15"
+    #wordsFile="cs"
+    #wordsFile="de"
+    #wordsFile="en"
+    if len(sys.argv) > 1:
+        wordsFile=sys.argv[1]
+    wcount=5
+    if len(sys.argv) > 2:
+        wcount=int(sys.argv[2])
+    lang= re.sub('[^a-z]+', '', wordsFile)
+    print("lang is "+lang)
+    caches.loadCache(lang)
+    words=generateWords.readWorlist(wordsFile)
+    random.shuffle(words)
+    desk=generateWords.generate(words, wcount)
+    for wwp in desk.wordsWithPlacement:
+        word=wwp.word
+        translatedId=caches.getTranslated(lang, word)
+        explanationFilesTransalted=caches.getFilesFromTransaltedAiExplainCache(lang, translatedId)
+        if not explanationFilesTransalted:
+            caches.explainToCache(lang, translatedId)
+            explanationFilesTransalted=caches.getFilesFromTransaltedAiExplainCache(lang, translatedId)
+        for file in explanationFilesTransalted:
+            print(file)
+            print(pathlib.Path(file).read_text())
+        explanationImages=caches.getFilesFromAiImageCache(translatedId)
+        if not explanationImages:
+            caches.imageToCache(lang, translatedId)
+            explanationImages=caches.getFilesFromAiImageCache(translatedId)
+        for file in explanationImages:
+            print(file)
+            show_image.display_image(file)
+    #cheat(desk)
+    #desk.prettyPrint()
+    print()
+    desk.hideAll()
+    desk.prettyPrint()
+    qhelp="help exit L ? ?n ?? (? ?n ?? I In II T G Tn TT GG newI newT delIn delTn)[a-z]"
+    history=[];
+    comandsUsage = {}
     print(qhelp)
-    cmd=line.strip()
-    history.append(cmd);
-    if 'exit' == cmd:
-        break
-    if 'help' == cmd:
-        # each cmd with [a-z] should write length of that word
-        print("Type `exit` to gave up (solution and statistics will be printed)");
-        print("I[a-z] to show next image (from all) for given word")
-        print("T[a-z] to show next hint (from all) for given word")
-        print("G[a-z] to show next hint (from all) for given word in external window")
-        print("Inumber[a-z] to show Nth image (from all) for given word")
-        print("Tnumber[a-z] to show Nth hint (from all) for given word")
-        print("Gnumber[a-z] to show Nth hint (from all) for given word in external window")
-        print("II[a-z] to show all images for given word")
-        print("TT[a-z] to show all texts for given word")
-        print("GG[a-z] to show all texts for given word in external window")
-        generateWords.reusableHelp()
-        print("newI[a-z] to generate and add new image for given word. Use all instead of a-z to generate all")
-        print("newT[a-z] to generate and add new text for given word. Use all instead of a-z to generate all")
-        print("delInumber[a-z] to remove Nth image. Check by In before")
-        print("delTnumber[a-z] to remove Nth text. Check by Tn before")
-        print("everything else is considered as guess")
-print("solved crossword:")
+    for line in sys.stdin:
+        print(qhelp)
+        cmd=line.strip()
+        history.append(cmd);
+        if 'exit' == cmd:
+            desk.gaveUp()
+            desk.prettyPrint()
+            break
+        if 'help' == cmd:
+            # each cmd with [a-z] should write length of that word
+            print("Type `exit` to gave up (solution and statistics will be printed)");
+            print("I[a-z] to show next image (from all) for given word")
+            print("T[a-z] to show next hint (from all) for given word")
+            print("G[a-z] to show next hint (from all) for given word in external window")
+            print("Inumber[a-z] to show Nth image (from all) for given word")
+            print("Tnumber[a-z] to show Nth hint (from all) for given word")
+            print("Gnumber[a-z] to show Nth hint (from all) for given word in external window")
+            print("II[a-z] to show all images for given word")
+            print("TT[a-z] to show all texts for given word")
+            print("GG[a-z] to show all texts for given word in external window")
+            generateWords.reusableHelp()
+            print("newI[a-z] to generate and add new image for given word. Use all instead of a-z to generate all")
+            print("newT[a-z] to generate and add new text for given word. Use all instead of a-z to generate all")
+            print("delInumber[a-z] to remove Nth image. Check by In before")
+            print("delTnumber[a-z] to remove Nth text. Check by Tn before")
+            print("everything else is considered as guess")
+        if generateWords.reusableRepl(cmd, desk):
+            continue
+
+if __name__ == "__main__":
+    main()
