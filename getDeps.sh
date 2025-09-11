84094e488abb06de9497632682c952b3a1f3ad27
Jiri Vanek <jvanek@redhat.com>
2025-09-11 17:48:39 +0200

    Forgotten file in forgotten revision

--- getDeps.sh
+++ getDeps.sh
@@ -18,16 +18,62 @@ else
   SUDO=
 fi
 
-# To tun later container with gui:
+# To run later container with gui:
 # xhost +"local:podman@" #<- normal user !!! mandatory
 # GUI_PART="-v /tmp/.X11-unix:/tmp/.X11-unix:ro -e \"DISPLAY\" --security-opt label=type:container_runtime_t "
 # podman run $GUI_PART  -e DISPLAY=$DISPLAY  -ti  worstcrosswords  python ...
 if [ "x$CONTAINER_BUILD" == "xTrue" ] ; then
+  echo "Building containres."
+  echo "1: emptyone, where you ahve to mount local cache, as -v=your_local_dir_with_ai_cahe:/home/game/WorstCrossWords/cache"
+  echo "eg  -v=./cache:/home/game/WorstCrossWords/cache"
+  echo "To run this with gui, you must: "
+  echo 'xhost +"local:podman@" #<- normal user !!! mandatory'
+  echo 'GUI_PART="-v /tmp/.X11-unix:/tmp/.X11-unix:ro -e \"DISPLAY\" --security-opt label=type:container_runtime_t"'
+  echo "eg:"
+  echo 'podman run $GUI_PART  -e DISPLAY=$DISPLAY  -v=./cache:/home/game/WorstCrossWords/cache -ti  worstcrosswords_empty python show_image.py getDeps.sh'
+  echo "and you should see exempalr gui window. All cmds will do: caches.py explain.py game.py generateImage.py generateWords.py show_image.py show_images.py shuffle.pyt ranslate.py"
+
+  echo "primarily 'game.py' of course"
+  rm WorstCrossWords*.tar
   set -e
-  podman build --tag worstcrosswords .
-  podman save -o WorstCrossWords.tar worstcrosswords
+  podman build --tag worstcrosswords_base .
+  echo "FROM worstcrosswords_base
+RUN mkdir cache
+" > empty
+  podman build --file empty --tag worstcrosswords_empty .
+  podman save -o WorstCrossWords_empty.tar worstcrosswords_empty
+  rm empty
+  if [ "$#" -eq 0 ]; then
+    echo "No dirs/archives with caches provided, exiting"; 
+  else
+    echo "buiding image with read-only caches"
+    rm -rf cont_build_caches
+    mkdir cont_build_caches
+    echo "FROM worstcrosswords_base
+RUN mkdir cache
+" > full
+    i=0;
+    for arg in "$@"; do
+      let i=i+1
+      if [ -d $arg -a $arg == "cache" ] ; then
+         cp -rv $arg  cont_build_caches/cache$1
+      elif [ -d $arg ] ; then
+         cp -rv $arg/cache  cont_build_caches/cache$1
+      elif [ -f $arg ] ; then
+         tar -xvf $arg -C cont_build_caches cache
+         mv cache cache$1
+      fi
+      echo "COPY cont_build_caches/cache$1/* /home/game/WorstCrossWords/cache/" >> full
+    done
+    podman build --file full --tag worstcrosswords_full .
+    podman save -o WorstCrossWords_full.tar worstcrosswords_full
+    rm -rf cont_build_caches
+    rm full
+    echo "this container do not need (and can not have), and contain some caches already as simle (35GB..) demo"
+  fi
   exit 0
 fi
+
 
 if [ "x$SELF_INIT" == "xTrue" ] ; then
   $SUDO dnf install -y git
